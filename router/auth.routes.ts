a7f7221dbeb071e1574b8a073be9427449e4dc0b
Valhalla-Wait <60570103+Valhalla-Wait@users.noreply.github.com>
2022-11-10 22:44:24 +0300

    add_login

--- router/auth.routes.ts
+++ router/auth.routes.ts
@@ -2,6 +2,8 @@ import { Request, Response, Router } from "express"
 import User from "../models/User"
 import bcrypt from 'bcryptjs'
 import {check, validationResult} from 'express-validator'
+import jwt from 'jsonwebtoken'
+import config from 'config'
 
 const router = Router()
 
@@ -31,10 +33,42 @@ router.post('/registration',
       return res.status(400).json({message: `User with email ${email} already exist`})
     }
 
-    const hashPassword = await bcrypt.hash(password, 15)
+    const hashPassword = await bcrypt.hash(password, 8)
     const user = new User({email, password: hashPassword})
     await user.save()
     return  res.json({message: 'User was created'})
+  } catch (e) {
+    console.log(e)
+    res.send({message: 'Server error'})
+  }
+})
+
+router.post('/login', async (req:Request, res:Response) => {
+  try {
+
+    const {email, password}:RegistrationType = req.body
+
+    const user = await User.findOne({email})
+
+    if (!user) {
+      return res.status(400).json({message: `User not found`})
+    }
+    const isPassValid = bcrypt.compareSync(password, user.password)
+    if (!isPassValid) {
+      return res.status(400).json({message: "Invalid password"})
+    }
+    const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar
+      }
+    })
   } catch (e) {
     console.log(e)
     res.send({message: 'Server error'})
